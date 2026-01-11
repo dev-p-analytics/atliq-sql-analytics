983299fefc6b60d8100a9cf0b3c6e738e36eb87b
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-01-11 22:16:52 +1100

    Add Gross Sales detail query by product and fiscal year

--- 03_finance_analysis/finance_queries.sql
+++ 03_finance_analysis/finance_queries.sql
@@ -1,7 +1,6 @@
 -- ====================================================
 -- User-Defined Functions
 -- ====================================================
-
 -- Function: get_fiscal_year_au
 -- Purpose:
 -- Calculates fiscal year based on Australian Financial Year (1 July - 30 June)
@@ -20,7 +19,6 @@ END;
 -- ====================================================
 -- Finance Query: Sales for Customer Croma (FY 2022)
 -- ====================================================
-
 -- Business Question: 
 -- Retrieve monthly sales records for customer Croma (using customer code) within fiscal year 2022
 
@@ -33,7 +31,6 @@ ORDER BY date DESC;
 -- ====================================================
 -- User-Defined Functions
 -- ====================================================
-
 -- Function: get_fiscal_quarter_au
 -- Purpose:
 -- Calculates fiscal quarter based on Australian Financial Year (1 July - 30 June)
@@ -52,13 +49,43 @@ END
 -- ====================================================
 -- Finance Query: Sales for Customer Croma (FY 2022)
 -- ====================================================
-
 -- Business Question: 
 -- Retrieve monthly sales records for customer Croma (using customer code) within fiscal year 2022 AND within Quarter 4 (Q4 refers to April - June)
+	
 SELECT * FROM gdb0041.fact_sales_monthly
 WHERE
 	customer_code = 90002002 
 	AND fiscal_year_au(date) = 2022
 	AND get_fiscal_quarter_au(date) = 4
 ORDER BY date DESC;
+
+-- ====================================================
+-- Gross Sales Detail for Customer Croma
+-- ====================================================
+-- Business Question: 
+-- Retrieve product-level gross sales details, including qty, unit gross price, and total gross sales amount, for a customer within a fiscal year (Australian FY)
+
+SELECT
+sm.date, sm.product_code,
+p.product, p.variant, sm.sold_quantity,
+gp.gross_price,
+(gp.gross_price * sm.sold_quantity) AS gross_sales_total
+
+FROM fact_sales_monthly sm
+JOIN dim_product p
+ON sm.product_code = p.product_code
+
+JOIN fact_gross_price gp
+
+ON 
+	gp.product_code = p.product_code AND
+    gp.get_fiscal_year_au= get_fiscal_year_au(sm.date)
+WHERE
+	customer_code = 90002002
+	AND	get_fiscal_year_au(sm.date) = 2021
+
+ORDER BY date ASC
+
+
+
 
