79ad9db8c959435caffd26645c3de0fbfb3b6325
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-01-11 21:13:47 +1100

    Add Australian fiscal quarter SQL function
    Implemented get_fiscal_quarter_au to help with identifying quarter terms of the input year

--- 03_finance_analysis/finance_queries.sql
+++ 03_finance_analysis/finance_queries.sql
@@ -2,7 +2,7 @@
 -- User-Defined Functions
 -- ====================================================
 
--- Function : get_fiscal_year_au
+-- Function: get_fiscal_year_au
 -- Purpose:
 -- Calculates fiscal year based on Australian Financial Year (1 July - 30 June)
 
@@ -18,7 +18,7 @@ BEGIN
 END;
 
 -- ====================================================
--- Finance Query: Sales for Customer Croma (FY 2021)
+-- Finance Query: Sales for Customer Croma (FY 2022)
 -- ====================================================
 
 -- Business Question: 
@@ -29,3 +29,36 @@ WHERE
 customer_code = 90002002 AND
 fiscal_year_au(date) = 2022
 ORDER BY date DESC;
+
+-- ====================================================
+-- User-Defined Functions
+-- ====================================================
+
+-- Function: get_fiscal_quarter_au
+-- Purpose:
+-- Calculates fiscal quarter based on Australian Financial Year (1 July - 30 June)
+
+CREATE FUNCTION `get_fiscal_quarter_au` (
+	calendar_date DATE
+) RETURNS INTEGER
+
+DETERMINISTIC
+BEGIN
+	DECLARE get_fiscal_quarter_au INT;
+	SET get_fiscal_quarter_au= QUARTER(DATE_ADD(calendar_date, INTERVAL 6 MONTH)); -- Quarter instead of year()
+	RETURN get_fiscal_quarter_au;
+END
+
+-- ====================================================
+-- Finance Query: Sales for Customer Croma (FY 2022)
+-- ====================================================
+
+-- Business Question: 
+-- Retrieve monthly sales records for customer Croma (using customer code) within fiscal year 2022 AND within Quarter 4 (Q4 refers to April - June)
+SELECT * FROM gdb0041.fact_sales_monthly
+WHERE
+	customer_code = 90002002 
+	AND fiscal_year_au(date) = 2022
+	AND get_fiscal_quarter_au(date) = 4
+ORDER BY date DESC;
+
