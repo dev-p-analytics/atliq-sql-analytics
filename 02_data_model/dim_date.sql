4c51eeea08c92068953a2487573db667507e5d07
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-01-19 11:25:48 +1100

    Added comments on usage of dim_date table

--- 02_data_model/dim_date.sql
+++ 02_data_model/dim_date.sql
@@ -1,3 +1,9 @@
+-- ====================================================
+-- Table Creation
+-- ====================================================
+-- Purpose:
+-- Created for performance optimisation, instead of calling get_fiscal_year_au function each time to reference fiscal_year
+
 CREATE TABLE `dim_date` (
   `calendar_date` date NOT NULL,
   `fiscal_year` year GENERATED ALWAYS AS (YEAR(`calendar_date` + INTERVAL 6 MONTH)) VIRTUAL,
