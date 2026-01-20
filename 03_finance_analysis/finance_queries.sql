d10c342cced9193f8398ff3ee749fad61373ff0b
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-01-20 18:41:32 +1100

    Add Stored Procedure for top markets by net sales

--- 03_finance_analysis/finance_queries.sql
+++ 03_finance_analysis/finance_queries.sql
@@ -298,3 +298,23 @@ CREATE VIEW `net_sales` AS
 SELECT *,
 	(1-post_invoice_discount_pct)*net_invoice_sales AS net_sales
 FROM sales_postinv_discount
+
+-- ====================================================
+-- Stored Procedure: Top Markets by Net Sales
+-- ====================================================
+-- Purpose: 
+-- Return top markets by inputting parameters of fiscal_year and number to limit by
+	
+CREATE PROCEDURE `get_top_n_markets_by_net_sales` (
+	in_fiscal_year INT,
+    in_top_n INT)
+BEGIN
+	SELECT 
+		market,
+		ROUND(SUM(net_sales)/1000000,2) AS net_sales_millions
+	FROM net_sales
+	WHERE fiscal_year = in_fiscal_year
+	GROUP BY market
+	ORDER BY net_sales_millions DESC
+LIMIT in_top_n;
+END
