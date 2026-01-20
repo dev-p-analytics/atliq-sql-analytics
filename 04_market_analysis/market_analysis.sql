4050119dfbaf59d92081e3de760e1a12c389fca3
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-01-20 18:43:45 +1100

    Add Stored Procedure for top markets by net sales

--- 04_market_analysis/market_analysis.sql
+++ 04_market_analysis/market_analysis.sql
@@ -30,3 +30,23 @@ BEGIN
 		SET out_badge = 'Silver';
 	END IF;
 END
+
+-- ====================================================
+-- Stored Procedure: Top Markets by Net Sales
+-- ====================================================
+-- Purpose: 
+-- Return top markets by inputting parameters of fiscal_year and number to limit by
+	
+CREATE PROCEDURE `get_top_n_markets_by_net_sales` (
+	in_fiscal_year INT,
+    in_top_n INT)
+BEGIN
+	SELECT 
+		market,
+		ROUND(SUM(net_sales)/1000000,2) AS net_sales_millions
+	FROM net_sales
+	WHERE fiscal_year = in_fiscal_year
+	GROUP BY market
+	ORDER BY net_sales_millions DESC
+LIMIT in_top_n;
+END
